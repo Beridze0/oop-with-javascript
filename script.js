94d16a2b7f29ef93ebb9be501eb8f642cdc6469e
Beridze0 <beridzedavit904@gmail.com>
2025-05-27 19:43:34 +0400

    challenge n1

--- script.js
+++ script.js
@@ -1,54 +1,76 @@
 'use strict';
 
-const Person = function (firstName, birthYear) {
-  //Instance properties
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+// const Person = function (firstName, birthYear) {
+//   //Instance properties
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
 
-  //Never create method insade Constructor
-  // this.calcAge = function () {
-  //   console.log(2037 - this.birthYear);
-  // };
+//   //Never create method insade Constructor
+//   // this.calcAge = function () {
+//   //   console.log(2037 - this.birthYear);
+//   // };
+// };
+
+// const jonas = new Person('Jonas', 1991);
+// console.log(jonas);
+
+// // 1. New {} is created
+// // 2. function is called, this = {} (1.)
+// // 3. {} linked to prototype
+// // 4. function automatically return {}
+
+// const matilda = new Person('Matilda', 2017);
+// const jack = new Person('jack', 1975);
+
+// console.log(jonas instanceof Person);
+
+// // Prototypes
+// console.log(Person.prototype);
+
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
+
+// jonas.calcAge();
+
+// Person.prototype.species = 'Homo Sapiens';
+
+// console.log(jonas, matilda);
+
+// console.log(jonas.__proto__.__proto__);
+
+// const arr = [3, 6, 6, 9, 4, 5, 6, 9, 3]; // new Array === []
+// console.log(arr.__proto__ === Array.prototype); //true
+// //arr.__proto__ includes all methods for example map(), reduce, filter, at, concat, fill, find etc.
+// console.log(arr.__proto__.__proto__);
+
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
+
+// console.log(arr.unique());
+
+// const h1 = document.querySelector('h1');
+// console.dir(h1);
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
 };
 
-const jonas = new Person('Jonas', 1991);
-console.log(jonas);
-
-// 1. New {} is created
-// 2. function is called, this = {} (1.)
-// 3. {} linked to prototype
-// 4. function automatically return {}
-
-const matilda = new Person('Matilda', 2017);
-const jack = new Person('jack', 1975);
-
-console.log(jonas instanceof Person);
-
-// Prototypes
-console.log(Person.prototype);
-
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
+Car.prototype.accelerate = function () {
+  this.speed = this.speed + 10;
+  console.log(`${this.make} going at ${this.speed}km/h`);
 };
 
-jonas.calcAge();
-
-Person.prototype.species = 'Homo Sapiens';
-
-console.log(jonas, matilda);
-
-console.log(jonas.__proto__.__proto__);
-
-const arr = [3, 6, 6, 9, 4, 5, 6, 9, 3]; // new Array === []
-console.log(arr.__proto__ === Array.prototype); //true
-//arr.__proto__ includes all methods for example map(), reduce, filter, at, concat, fill, find etc.
-console.log(arr.__proto__.__proto__);
-
-Array.prototype.unique = function () {
-  return [...new Set(this)];
+Car.prototype.brake = function () {
+  this.speed = this.speed - 5;
+  console.log(`${this.make} going at ${this.speed}km/h`);
 };
 
-console.log(arr.unique());
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
 
-const h1 = document.querySelector('h1');
-console.dir(h1);
+bmw.accelerate();
+bmw.brake();
+mercedes.brake();
