ab2ea23192e9433db6ae32380e3b3b47058a15b4
Beridze0 <beridzedavit904@gmail.com>
2025-05-27 18:33:01 +0400

    creating constructor function

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  //Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //Never create method insade Constructor
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// 1. New {} is created
+// 2. function is called, this = {} (1.)
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('jack', 1975);
+
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
