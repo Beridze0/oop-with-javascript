a163498e39f54dd1ca50a082c92d3fd29c2aafb5
Beridze0 <beridzedavit904@gmail.com>
2025-05-28 21:30:44 +0400

    challenge 3

--- script.js
+++ script.js
@@ -236,30 +236,71 @@
 // console.log(bmw.speedUS);
 // bmw.speedUS = 50;
 
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
+
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
+
+// const Student = function (firstName, birthYear, course) {
+//   Person.call(this, firstName, birthYear);
+//   this.course = course;
+// };
+
+// //Linking prototypes
+// Student.prototype = Object.create(Person.prototype);
+
+// Student.prototype.introduce = function () {
+//   console.log(`My name is ${this.firstName} and I study ${this.course}`);
+// };
+
+// const mike = new Student('Mike', 2020, 'Computer Science');
+// console.log(mike);
+// mike.introduce();
+// mike.calcAge();
+
+// Student.prototype.constructor = Student;
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
 };
 
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
+Car.prototype.accelerate = function () {
+  this.speed = this.speed + 10;
+  console.log(`${this.make} going at ${this.speed}km/h`);
 };
 
-const Student = function (firstName, birthYear, course) {
-  Person.call(this, firstName, birthYear);
-  this.course = course;
+Car.prototype.brake = function () {
+  this.speed = this.speed - 5;
+  console.log(`${this.make} going at ${this.speed}km/h`);
 };
 
-//Linking prototypes
-Student.prototype = Object.create(Person.prototype);
-
-Student.prototype.introduce = function () {
-  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
 };
 
-const mike = new Student('Mike', 2020, 'Computer Science');
-console.log(mike);
-mike.introduce();
-mike.calcAge();
+//Linking
+EV.prototype = Object.create(Car.prototype);
 
-Student.prototype.constructor = Student;
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} going at ${this.speed}km/h, with charge of ${this.charge}%`
+  );
+};
+
+const tesla = new EV('Tesla', 120, 23);
+
+tesla.chargeBattery(90);
+tesla.brake();
+tesla.accelerate();
